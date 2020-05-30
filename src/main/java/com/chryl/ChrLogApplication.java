6ffd4b2707f6e4aa92db21485e630a2da63ed143
chryl <cyl5yy412@gmail.com>
2020-05-30 19:48:45 +0800

    Initial commit

---
+++ src/main/java/com/chryl/ChrLogApplication.java
@@ -0,0 +1,34 @@
+package com.chryl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class ChrLogApplication {
+
+    public static void main(String[] args) {
+        log.info("zZZZZZZZZZZZ");
+        log.warn("mmmmmm");
+        log.warn("mmmmmm");
+        log.warn("mmmmmm");
+        log.warn("mmmmmm");
+        log.warn("mmmmmm");
+        log.warn("mmmmmm");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        log.info("zZZZZZZZZZZZ");
+        SpringApplication.run(ChrLogApplication.class, args);
+    }
+
+}
