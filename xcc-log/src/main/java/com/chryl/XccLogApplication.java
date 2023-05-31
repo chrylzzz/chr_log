0be30f40e07eec13b9dc6e2a2539d211b12d786d
chryl <cyl5yy412@gmail.com>
2023-05-31 21:43:02 +0800

    彩色日志,多环境logback日志

---
+++ xcc-log/src/main/java/com/chryl/XccLogApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XccLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XccLogApplication.class, args);
+	}
+
+}
---
+++ xcc-log/src/main/java/com/chryl/controller/XccLogController.java
@@ -0,0 +1,20 @@
+package com.chryl.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Chr.yl on 2021/3/15.
+ *
+ * @author Chr.yl
+ */
+@Slf4j
+@RestController
+@RequestMapping("xcc")
+public class XccLogController {
+
+    @GetMapping("log")
+    public void show() {
+        log.info("----------------------");
+    }
+}
