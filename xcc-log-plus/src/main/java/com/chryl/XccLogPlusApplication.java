aaf5f5c90b45e1b3e644a8991f56b8180f563e70
chryl <cyl5yy412@gmail.com>
2023-06-05 16:06:25 +0800

    指定加载 spring boot 配置文件使用配置根据环境自动装配

---
+++ xcc-log-plus/src/main/java/com/chryl/XccLogPlusApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XccLogPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XccLogPlusApplication.class, args);
+	}
+
+}
