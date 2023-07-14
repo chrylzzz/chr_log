f332ce2730645eebc9571783e48bca58c36e622d
chryl <cyl5yy412@gmail.com>
2023-07-14 18:07:13 +0800

    测试fastjson2 下划线和驼峰互转

--- xcc-log-plus/src/main/java/com/chryl/XccLogPlusApplication.java
+++ xcc-log-plus/src/main/java/com/chryl/XccLogPlusApplication.java
@@ -1,13 +1,18 @@
 package com.chryl;
 
+import com.chryl.config.ChrylConfigProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ChrylConfigProperty.class})
+@ComponentScan("com.chryl.**")
 public class XccLogPlusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(XccLogPlusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(XccLogPlusApplication.class, args);
+    }
 
 }
--- xcc-log-plus/src/main/java/com/chryl/controller/XccLogPlusController.java
+++ xcc-log-plus/src/main/java/com/chryl/controller/XccLogPlusController.java
@@ -1,7 +1,16 @@
 package com.chryl.controller;
 
+import com.alibaba.fastjson.PropertyNamingStrategy;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
+import com.chryl.config.ChrylConfigProperty;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by Chr.yl on 2021/3/15.
@@ -12,9 +21,41 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("xcc")
 public class XccLogPlusController {
+    @Autowired
+    private ChrylConfigProperty chrylConfigProperty;
+
+    @GetMapping("chryl")
+    public void chryl(HttpServletResponse response) throws InterruptedException, IOException {
+        String loadYml = JSON.toJSONString(chrylConfigProperty, JSONWriter.Feature.PrettyFormat);
+        log.info("loadYml : {}", loadYml);
+        response.getWriter().write(loadYml);
+        response.flushBuffer();
+        response.getWriter().flush();
+        response.getWriter().close();
+    }
+
+    @GetMapping("show")
+    public Object show() {
+
+        SerializeConfig config = new SerializeConfig();
+        /*
+        有四种序列化方式。
+        CamelCase策略，Java对象属性：personId，序列化后属性：persionId – 实际只改了首字母 大写变小写
+        PascalCase策略，Java对象属性：personId，序列化后属性：PersonId – 实际只改了首字母 小写变大写
+        SnakeCase策略，Java对象属性：personId，序列化后属性：person_id --大写字母前加下划线
+        KebabCase策略，Java对象属性：personId，序列化后属性：person-id -大写字母前加减号
+         */
+//        config.propertyNamingStrategy = PropertyNamingStrategy.SnakeCase;
+        config.propertyNamingStrategy = PropertyNamingStrategy.KebabCase;
+//        String loadYml = com.alibaba.fastjson.JSON.toJSONString(new IVREvent("asksaklf"), config);
+        String loadYml = com.alibaba.fastjson.JSON.toJSONString(chrylConfigProperty, config);
+//        String loadYml = JSON.toJSONString(IVRInit.CHRYL_CONFIG_PROPERTY, JSONWriter.Feature.PrettyFormat);
+        return loadYml;
+
+    }
 
     @GetMapping("log")
-    public void show() {
+    public void log() {
         log.info("----------------------");
     }
 }
